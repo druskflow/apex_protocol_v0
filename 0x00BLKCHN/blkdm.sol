2efa6f2a46f97df9b1833d367ed0563667698763
LaxrLee <stangathagu95@gmail.com>
2022-07-13 12:23:58 +0300

    hash

--- 0x00BLKCHN/blkdm.sol
+++ 0x00BLKCHN/blkdm.sol
@@ -39,3 +39,43 @@
 // Anyone can then verify this new transaction hash woth your public key.
 
 // Private key creates Public key which creates Address
+
+// Blockchain nodes keep lists of the transactions that occur
+
+// Consensus/ Proof of Stake/ Proof of Work
+// Consensus is the mechanism used to agree on the state of a blockchain. Majority rule(example)
+// Chain Seection and Sybil Resistance(used by btc and eth)
+// Proof of work is known as Sybil resistance mechanism as it defines a way to figure out who is the block author.
+
+// Proof of work - a single node has to go through a very computational expensive process called mining to figure out the real owner.
+// No matter what pseudoanonymous accounts are only one is picked
+
+// Chain Selection, how to know which block to pick
+// Nakamoto Coonsensus - is a combination of proof of work and longest chain rule. The decentralized network decides whichever blockchain has
+// the longest chain or the highest number of blocks is going to be the chain to be used 
+
+
+
+// Attacks 
+// Sybil Attack - when a user creates a whole bunch of pseudoaccounts to try and influence the network. 
+// 51% Attack - if oyu have the longest chain and have more than 51% match on the network you can fork the network and bring it to your chain.
+// The bigger the network the more decentralized it is and the more secure it becomes. Run a node and contribute to the network
+
+// Proof of Work costs a lot of energy
+
+// Proof of Stake(PoS) - more environmental friendly as it does not use a lot of energy. Prof of Stake don't need to solve the problems
+// they put up collateral that they will behave honestly.
+// Proof of Stake nodes put up collateral as a syvil resistance mechanism
+// Miners are known as validators.
+
+// Randomness - These blockchains are deterministic and by law randomness cannot exist in a deterministic system
+// How to choose the ranomb validators. It chancges from blockchain to blockchain. 
+
+
+// Sharding - c a solution to the scalability problem. As more people getting into the network the gas feessky rocket
+// A sharded blockchaion b=means it is going to be a blockchain of blockchains. There will be a mainchain that will coordinate everything 
+// amongst several chains that hook into this mainchain. This means that there is more chains for more people to make transactions on 
+// effectively increasing the amount of bock spaces. 
+
+// Layer 1 - Base layer blockchain implementation e.g btc, eth, avalanche
+// Layer 2 - Applicatiyons built on a layer 1 e.g chainlink, arbitrum and optimism
--- 0x00BLKCHN/gas.sol
+++ 0x00BLKCHN/gas.sol
@@ -1,3 +1,26 @@
+// chain link is the most popular and powerful oracle network
+
+// hybrid smartcontract - are smart contracts specifically with an off-chain component. 
+// It combines decentralized on-chain settlement layer and any decentralized external
+// off-chain data or computation 
+
+// Chainlink provides data ands external computation to smart contracts 
+
+// The individuals that make up blockchain are known as node operators and they are the 
+// indeppendent individuals running the software that connects the whole blockchain together. 
+ 
+
+// Decentralized
+// Transparent
+// Speed
+// Immutable
+// Remove Counterparty Risk
+// Allow for trust minimized agreements
+// Hybrid Smart Contracts combine on and off-chain
+
+
+// Freedom and Trustless
+
 // Gas: a unit of computational measure. The more computation a transaction uses 
 // the more gas you have to pay for
 
