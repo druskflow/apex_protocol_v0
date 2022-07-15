5b5436d5d618ca466a849c241f106d4f50d94215
LaxrLee <stangathagu95@gmail.com>
2022-07-15 15:22:10 +0300

    js

---
+++ jvsptIBM/intro.js
@@ -0,0 +1,28 @@
+// javascript is a free, client-side scripting language that lets you add interactivitu to HTML pages.
+// Client side means js runs on browser and not the server side
+// functions: dynamically add, edit and remove HTML elements and their values
+//            validate web forms before submission and create cookies to store & retrieve data on a user's computer for future visits
+
+
+// local
+{/* <script type="text/javascript">
+     example statement here 
+</script> */}
+
+// external
+{/* <script type="text/javascript" src="path/to/javascript.js"></script> */}
+// uses of externaljavascript fiiles:
+        // search engines can crawl and index your website faster if there is less code in your HTML page
+
+// A funciton with no return value will return undefined.
+// If you declare a variable and don't initialize it, it returns the undefined value.
+// If you query a non-exixtent array element or object property, an undefined value is returned.
+
+
+// variable declaration 
+{/*var cost;
+var cost, profit;
+var cost = 120;
+var cost = 120, profit = 77;*/}
+// if a variable is not declared explicitly using var then javascript will automatically treat it as a global variable
+// a variable must be identified with a unique name
